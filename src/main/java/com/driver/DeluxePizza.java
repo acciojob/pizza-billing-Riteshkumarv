56e9a1ed1c828461c2b0427d841f5d558f7995bc
ritesh kumar <Ritesh74461@gmail.com>
2023-03-18 09:41:04 +0530

    assignment sublited

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,12 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        if(isVeg){
+            SetB("Price Of The DeluxePizza: 450\n");
+            SetP(450);
+        }else{
+            SetB("Price Of The DeluxePizza: 600\n");
+            SetP(600);
+        }
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,25 +6,68 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    public boolean isChess = false;
+    public boolean addTopp = false;
+
+    public boolean beg = false;
+    public int getP(){
+        return price;
+    }
+    public void SetP(int price){
+        this.price += price;
+    }
+    public String getB(){
+        return bill;
+    }
+    public void SetB(String bill){
+        this.bill += bill;
+    }
+    public boolean getV(){
+        return isVeg;
+    }
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg == true){
+            bill = "Base Price Of The Pizza: 300\n";
+            price = 300;
+        }else{
+            bill = "Base Price Of The Pizza: 400\n";
+            price = 400;
+        }
     }
-
     public int getPrice(){
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!isChess) {
+            SetB("Extra Cheese Added: 80\n");
+            SetP(80);
+            isChess = true;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!addTopp) {
+            if (getV()) {
+                SetB("Extra Toppings Added: 70\n");
+                SetP(70);
+            } else {
+                SetB("Extra Toppings Added: 120\n");
+                SetP(120);
+            }
+            addTopp = true;
+        }
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void addTakeaway() {
+        if(!beg) {
+            SetB("Paperbag Added: 20\n");
+            beg = true;
+        }
     }
 
     public String getBill(){
