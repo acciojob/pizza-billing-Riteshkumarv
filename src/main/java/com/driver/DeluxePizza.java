5383f5187affd94151156c56f978be6d4ed06a31
ritesh kumar <Ritesh74461@gmail.com>
2023-03-18 09:53:31 +0530

    new

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,12 +4,8 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        if(isVeg){
-            SetB("Price Of The DeluxePizza: 450\n");
-            SetP(450);
-        }else{
-            SetB("Price Of The DeluxePizza: 600\n");
-            SetP(600);
-        }
+
+        addExtraCheese();
+        addExtraToppings();
     }
 }
